c95c5ea20236e3ef70fa4a789d6491fbfd7e8f90
Rick Mastry <jahrik@hotmail.com>
2021-07-25 21:37:42 -0400

    Updates

---
+++ precache-manifest.312b1f32eddae6888c5dded7291dcf11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8eb786fa35b1edc67d60212a2a6e70b",
+    "url": "/Tic_Tac_Toe/index.html"
+  },
+  {
+    "revision": "7fbdda4c925a1aa842f9",
+    "url": "/Tic_Tac_Toe/static/css/main.ca4f820b.chunk.css"
+  },
+  {
+    "revision": "ca9f4ae04c4dfc6c49c9",
+    "url": "/Tic_Tac_Toe/static/js/2.9b9cc868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic_Tac_Toe/static/js/2.9b9cc868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fbdda4c925a1aa842f9",
+    "url": "/Tic_Tac_Toe/static/js/main.1b3b388c.chunk.js"
+  },
+  {
+    "revision": "983a621b7bbcc2dd58da",
+    "url": "/Tic_Tac_Toe/static/js/runtime-main.df7e0b54.js"
+  }
+]);
